5166ed51ddd93772d556cffe30bacf35cc87f812
Brian Davies <brian@briandavies.me>
2016-10-23 00:26:15 -0600

    Refactor UserPolicy spec

--- spec/policies/user_policy_spec.rb
+++ spec/policies/user_policy_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe UserPolicy, type: :policy do
+  it { is_expected.to permit_action(:create) }
+
   context "when user is record" do
     let(:record) { user }
 
-    it { is_expected.to permit_action(:create) }
     it { is_expected.to permit_action(:update) }
     it { is_expected.to permit_action(:destroy) }
 
@@ -12,13 +13,13 @@ RSpec.describe UserPolicy, type: :policy do
       persist_record_and_user
 
       it "includes only the authenticated user" do
-        expect(subject.scope).to eq [user]
+        create(:user)
+        expect(resolved_scope).to eq [user]
       end
     end
   end
 
   context "when user and record are different" do
-    it { is_expected.to permit_action(:create) }
     it { is_expected.to forbid_action(:update) }
     it { is_expected.to forbid_action(:destroy) }
 
@@ -26,8 +27,7 @@ RSpec.describe UserPolicy, type: :policy do
       persist_record_and_user
 
       it "includes only the authenticated user" do
-        create(:user)
-        expect(subject.scope).to eq [user]
+        expect(resolved_scope).to eq [user]
       end
     end
   end
