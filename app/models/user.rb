aa47a25ccd9bc9b3602785d7e560105d5fcd9594
Brian Davies <brian@briandavies.me>
2016-10-22 21:04:21 -0600

    Minor User clean up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
-require 'activevalidators'
-
 class User < ApplicationRecord
   has_secure_password
 
   validates :email,
     presence: true,
-    uniqueness: { case_insensitive: false },
+    uniqueness: { case_insensitive: true },
     email: { message: "invalid email format" }
 end
