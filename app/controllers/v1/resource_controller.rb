19496371ccd460c2d43ec08413fccd10a9bb00bd
Brian Davies <brian@briandavies.me>
2016-10-31 21:44:12 -0600

    Namespace ResourceController in V1

--- app/controllers/v1/resource_controller.rb
+++ app/controllers/v1/resource_controller.rb
@@ -1,12 +1,15 @@
-class ResourceController < JSONAPI::ResourceController
-  include Pundit::ResourceController
-  include Doorkeeper::Rails::Helpers
+module V1
+  class ResourceController < JSONAPI::ResourceController
+    include Pundit::ResourceController
+    include Doorkeeper::Rails::Helpers
+    include ActionController::Head
 
-  before_action :doorkeeper_authorize!
+    before_action :doorkeeper_authorize!
 
-  protected
+    protected
 
-  def current_user
-    User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+    def current_user
+      User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+    end
   end
 end
