ccc44d5771c2e3e0de2636675350e91ccdeb3f89
Brian Davies <brian@briandavies.me>
2016-11-14 21:48:12 -0700

    Improve UserController

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -2,9 +2,11 @@ module V1
   class UsersController  < V1::ResourceController
     skip_before_action :doorkeeper_authorize!, only: [:create]
 
-    prepend_before_action :handle_me_parameter, only: [:show]
+    prepend_before_action :_replace_me_with_current_user_id, only: [:show]
 
-    def handle_me_parameter
+    private
+
+    def _replace_me_with_current_user_id
       return unless current_user
       return unless params[:id] == "me"
 
