9a21e2a4c280b86edd6c7b51df50e405c3418a05
Brian Davies <brian@briandavies.me>
2016-12-21 20:59:20 -0700

    Add HasUrl

---
+++ app/models/concerns/has_url.rb
@@ -0,0 +1,21 @@
+module HasUrl
+  extend ActiveSupport::Concern
+
+  class_methods do
+    def url_normalize attribute, options = { default_protocol: "http" }
+      define_method "normalize_#{attribute}_as_url".to_sym do
+        attribute_change = public_send("#{attribute}_change")
+        new_value = attribute_change&.last
+        return unless new_value
+
+        new_value_is_missing_protocol = (new_value !~ /^A(http|ftp).*/)
+        return unless new_value_is_missing_protocol
+
+        public_send("#{attribute}=", "#{options[:default_protocol]}://#{new_value}")
+      end
+
+      before_validation "normalize_#{attribute}_as_url".to_sym
+    end
+  end
+
+end
