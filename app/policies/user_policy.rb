d882523cde27c3f01310499c174a5f481e99314e
Brian Davies <brian@briandavies.me>
2016-12-22 17:40:14 -0700

    Update UserPolicy for organzation memberships

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -4,7 +4,13 @@ class UserPolicy < ApplicationPolicy
   end
 
   def update?
-    user == record
+    case
+    when non_admin_user_is_changing_kind? then false
+    when user.application_admin? then true
+    when user_and_record_share_a_membership then true
+    when user == record then true
+    else false
+    end
   end
 
   def destroy?
@@ -13,7 +19,23 @@ class UserPolicy < ApplicationPolicy
 
   class Scope < Scope
     def resolve
-      scope.where(id: user.id)
+      return scope.all if user.application_admin?
+      scope
+        .joins(:memberships)
+        .where(memberships: { organization_id: scope_for(Organization).select(:id) })
     end
+  end
+
+  private
+
+  def non_admin_user_is_changing_kind?
+    if user == record
+      return user.kind_changed? && user.kind_was == "standard"
+    end
+    !user.application_admin? && record.kind_changed?
+  end
+
+  def user_and_record_share_a_membership
+    (user.organizations & record.organizations).any?
   end
 end
--- spec/policies/user_policy_spec.rb
+++ spec/policies/user_policy_spec.rb
@@ -1,33 +1,79 @@
 require 'rails_helper'
 
 RSpec.describe UserPolicy, type: :policy do
-  it { is_expected.to permit_action(:create) }
+
+  context "when user is application admin" do
+    before { user.kind = "application_admin" }
+
+    it { is_expected.to permit_action(:create) }
+    it { is_expected.to permit_action(:update) }
+    it { is_expected.to permit_action(:destroy) }
+  end
 
   context "when user is record" do
     let(:record) { user }
 
-    it { is_expected.to permit_action(:update) }
+    it { is_expected.to permit_action(:create) }
     it { is_expected.to permit_action(:destroy) }
 
-    describe "#scope" do
-      persist_record_and_user
+    context "when update includes kind" do
+      before { record.kind = "application_admin"}
+      it { is_expected.to forbid_action(:update) }
+    end
 
-      it "includes only the authenticated user" do
-        create(:user)
-        expect(resolved_scope).to eq [user]
-      end
+    context "when update does not include kind" do
+      it { is_expected.to permit_action(:update) }
     end
   end
 
-  context "when user and record are different" do
+  context "when user is member of same organization" do
+    persist_record_and_user
+
+    before do
+      organization = create(:organization)
+      create(:membership, user: user, organization: organization)
+      create(:membership, user: record, organization: organization)
+    end
+
+    it { is_expected.to permit_action(:create) }
+    it { is_expected.to permit_action(:destroy) }
+
+    context "when update includes kind" do
+      before { record.kind = "application_admin"}
+      it { is_expected.to forbid_action(:update) }
+    end
+
+    context "when update does not include kind" do
+      it { is_expected.to permit_action(:update) }
+    end
+  end
+
+  context "when user is unrelated" do
+    it { is_expected.to permit_action(:create) }
     it { is_expected.to forbid_action(:update) }
     it { is_expected.to forbid_action(:destroy) }
+  end
 
-    describe "#scope" do
-      persist_record_and_user
+  describe "#scope" do
+    persist_record_and_user
 
-      it "includes only the authenticated user" do
-        expect(resolved_scope).to eq [user]
+    before { create(:membership, user: user, organization: organization) }
+    let!(:organization) { create(:organization) }
+
+    let!(:shared_membership) { create(:membership, organization: organization).user }
+    let!(:unrelated) { create(:user) }
+
+    context "when non-admin" do
+      it "includes the organizations the user is a member of" do
+        expect(resolved_scope).to eq [user, shared_membership]
+      end
+    end
+
+    context "when user is applciation administrator" do
+      before { user.update kind: "application_admin" }
+
+      it "includes all organizations" do
+        expect(resolved_scope.to_set).to eq User.all.to_set
       end
     end
   end
